c1d5825dde2cd38857e2de718f5a169366c9235b
MarioRD64 <mario2011rd@gmail.com>
2025-03-01 16:57:25 +0100

    reimported scripts

---
+++ app.py
@@ -0,0 +1,21 @@
+import gevent.monkey
+import os
+gevent.monkey.patch_all()
+
+from flask import Flask
+from flask_socketio import SocketIO
+
+print("✅ Flask está iniciando...")  # 🔥 Agregamos esto para ver si el código se ejecuta
+
+app = Flask(__name__)
+
+socketio = SocketIO(app, async_mode='gevent')
+
+@app.route('/')
+def home():
+    return "¡Hola, Bienvenido a Mi Concursillo!"
+
+if __name__ == '__main__':
+    print("🚀 Ejecutando Flask en el puerto 5000...")  # 🔥 Mensaje de prueba
+    port = int(os.environ.get("PORT", 5000))  # Asegura que use un puerto dinámico
+    socketio.run(app, host='0.0.0.0', port=port)
